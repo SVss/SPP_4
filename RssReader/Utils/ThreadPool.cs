af6c45825b585b0fe77c29ec4d7a2f1a8635c5eb
SVss <savionak@gmail.com>
2016-11-11 13:43:55 +0300

    Add ThreadPool

---
+++ RssReader/Utils/ThreadPool.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RssReader.Utils
+{
+    class ThreadPool: IDisposable
+    {
+        
+        // Public
+
+        public ThreadPool(int threadsCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        ~ThreadPool()
+        {
+            Dispose(false);
+        }
+
+        public void QueueTask(Action task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Internals
+
+        private void Dispose(bool safe)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void WorkerBody(object actionQueue)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}

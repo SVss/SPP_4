6a6220c1926e66e433bed48ecb7ec0babc73d284
SVss <savionak@gmail.com>
2016-12-06 20:03:09 +0300

    [FilterModel]: Implement XML methods
    * FromXmlElement
    * ToXmlElement

--- RssReader/Model/FilterModel.cs
+++ RssReader/Model/FilterModel.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Reflection;
 using System.Xml;
+using RssReader.Utils;
 
 namespace RssReader.Model
 {
@@ -21,12 +22,92 @@ namespace RssReader.Model
 
         public static FilterModel FromXmlElement(XmlElement xe)
         {
-            throw new NotImplementedException();
+            if (xe == null)
+            {
+                throw new BadXmlException();
+            }
+
+            FilterModel result = null;
+
+            if (xe.Name == ConfigConsts.IncludeFilterTag)
+            {
+                result = new IncludeFilterModel();
+            }
+            else if (xe.Name == ConfigConsts.ExcludeFilterTag)
+            {
+                result = new ExcludeFilterModel();
+            }
+            else
+            {
+                throw new BadXmlException();
+            }
+
+            foreach (XmlElement method in xe.ChildNodes)
+            {
+                if (method.Name == ConfigConsts.AndFilterMethodTag)
+                {
+                    foreach (XmlElement itm in method.ChildNodes)
+                    {
+                        result.AndList.Add(itm.InnerText);
+                    }
+                }
+                else if (method.Name == ConfigConsts.OrFilterMethodTag)
+                {
+                    foreach (XmlElement itm in method.ChildNodes)
+                    {
+                        result.OrList.Add(itm.InnerText);
+                    }
+                }
+                else
+                {
+                    throw new BadXmlException();
+                }
+            }
+
+            return result;
         }
 
-        public XmlElement ToXmlElement()
+        public XmlElement ToXmlElement(XmlDocument document)
         {
-            throw new NotImplementedException();
+            string name = string.Empty;
+            if (this.Type == FilterType.Include)
+            {
+                name = ConfigConsts.IncludeFilterTag;
+            }
+            else if (this.Type == FilterType.Exclude)
+            {
+                name = ConfigConsts.ExcludeFilterTag;
+            }
+
+            XmlElement result = document.CreateElement(name);
+
+            if (AndList.Count > 0)
+            {
+                var list = document.CreateElement(ConfigConsts.AndFilterMethodTag);
+                foreach (string s in AndList)
+                {
+                    var itm = document.CreateElement(ConfigConsts.FilterItemTag);
+                    itm.InnerText = s;
+
+                    list.AppendChild(itm);
+                }
+                result.AppendChild(list);
+            }
+
+            if (OrList.Count > 0)
+            {
+                var list = document.CreateElement(ConfigConsts.OrFilterMethodTag);
+                foreach (string s in OrList)
+                {
+                    var itm = document.CreateElement(ConfigConsts.FilterItemTag);
+                    itm.InnerText = s;
+
+                    list.AppendChild(itm);
+                }
+                result.AppendChild(list);
+            }
+
+            return result;
         }
 
         // Protected
@@ -76,7 +157,7 @@ namespace RssReader.Model
 
         // Internals
 
-        private IncludeFilterModel()
+        protected internal IncludeFilterModel()
         {
             this.Type = FilterType.Include;
         }
@@ -94,7 +175,7 @@ namespace RssReader.Model
 
         // Internals
 
-        private ExcludeFilterModel()
+        protected internal ExcludeFilterModel()
         {
             this.Type = FilterType.Exclude;
         }

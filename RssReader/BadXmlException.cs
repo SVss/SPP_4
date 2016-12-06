d1434cd88e04e619807e9606423d165ab818595a
SVss <savionak@gmail.com>
2016-12-06 18:33:41 +0300

    Add XML tags and BadXmlException

---
+++ RssReader/BadXmlException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RssReader
+{
+    [Serializable]
+    internal class BadXmlException : Exception
+    {
+        public BadXmlException()
+        {
+        }
+
+        public BadXmlException(string message) : base(message)
+        {
+        }
+
+        public BadXmlException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected BadXmlException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ RssReader/Utils/ConfigConsts.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace RssReader.Utils
+{
+    internal static class ConfigConsts
+    {
+        public static string ConfigPath =>
+            Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\config.xml";
+
+        public static string RootTag => "root";
+
+        public static string UserTag => "user";
+        public static string UserNameAttr => "name";
+        public static string ThreadsCountTag => "threads";
+
+        public static string ChannelsListTag => "feeds";
+        public static string ChannelTag => "channel";
+
+        public static string FiltersListTag => "filters";
+        public static string IncludeFilterTag => "include";
+        public static string ExcludeFilterTag => "exclude";
+        public static string FilterItemTag => "item";
+    }
+}

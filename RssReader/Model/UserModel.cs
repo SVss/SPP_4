60c1ce98cb9093c5eb49b52c63012a678635370b
SVss <savionak@gmail.com>
2016-12-06 21:04:59 +0300

    [UserModel]: Implement XML methods
    * FromXmlElement
    * ToXmlElement

--- RssReader/Model/UserModel.cs
+++ RssReader/Model/UserModel.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Xml;
+using RssReader.Utils;
 
 namespace RssReader.Model
 {
-    class UserModel
+    public class UserModel
     {
         public string Name { get; private set; }
         public int ThreadsCount { get; set; }
 
         public List<FeedModel> FeedsList { get; } = new List<FeedModel>();
-
-        public FilterModel IncludFilter = null;
-        public FilterModel ExcledeFilter = null;
+        public List<FilterModel> FiltersList { get; private set; } = new List<FilterModel>();
 
         // Public
 
@@ -24,12 +23,79 @@ namespace RssReader.Model
 
         public static UserModel FromXmlElement(XmlElement xe)
         {
-            throw new NotImplementedException();
+            if (xe.Name != ConfigConsts.UserTag)
+            {
+                throw new BadXmlException();
+            }
+
+            string name;
+            int threadsCount = 0;
+
+            try
+            {
+                name = xe.Attributes[ConfigConsts.UserNameAttr].Value;
+                threadsCount = Convert.ToInt32(xe.Attributes[ConfigConsts.ThreadsCountTag].Value);
+            }
+            catch (Exception ex)
+            {
+                if (ex is XmlException || ex is FormatException || ex is OverflowException)
+                    throw new BadXmlException();
+
+                throw;
+            }
+            UserModel result = new UserModel(name, threadsCount);
+
+            // Read feeds
+            XmlElement child = xe.FirstChild as XmlElement;
+            if ((child == null) || (child.Name != ConfigConsts.ChannelsListTag))
+            {
+                throw new BadXmlException();
+            }
+
+            foreach (XmlElement channel in child.ChildNodes)
+            {
+                result.FeedsList.Add(FeedModel.FromXmlElement(channel));
+            }
+
+            // Read filters
+            child = child.NextSibling as XmlElement;
+            if ((child == null) || (child.Name != ConfigConsts.FiltersListTag))
+            {
+                throw new BadXmlException();
+            }
+
+            foreach (XmlElement filter in child.ChildNodes)
+            {
+                result.FiltersList.Add(FilterModel.FromXmlElement(filter));
+            }
+
+            return result;
         }
 
-        public XmlElement ToXmlElement()
+        public XmlElement ToXmlElement(XmlDocument document)
         {
-            throw new NotImplementedException();
+            XmlElement result = document.CreateElement(ConfigConsts.UserTag);
+
+            result.SetAttribute(ConfigConsts.UserNameAttr, Name);
+            result.SetAttribute(ConfigConsts.ThreadsCountTag, ThreadsCount.ToString());
+
+            // Write feeds
+            var child = document.CreateElement(ConfigConsts.ChannelsListTag);
+            foreach (FeedModel feed in FeedsList)
+            {
+                child.AppendChild(feed.ToXmlElement(document));
+            }
+            result.AppendChild(child);
+
+            // Write filters
+            child = document.CreateElement(ConfigConsts.FiltersListTag);
+            foreach (FilterModel filter in FiltersList)
+            {
+                child.AppendChild(filter.ToXmlElement(document));
+            }
+            result.AppendChild(child);
+
+            return result;
         }
     }
 }

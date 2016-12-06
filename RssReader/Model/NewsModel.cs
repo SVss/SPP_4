742fc7388dd5f95d3febb6fdafca0543dfb0a005
SVss <savionak@gmail.com>
2016-12-06 20:29:11 +0300

    [NewsModel]: Change Link type
    * check link before initializing

--- RssReader/Model/NewsModel.cs
+++ RssReader/Model/NewsModel.cs
@@ -6,15 +6,21 @@ namespace RssReader.Model
     {
         public string Caption { get; private set; }
         public string Description { get; private set; }
-        public string Link { get; private set; }
+        public Uri Link { get; private set; }
 
         public string FullText => Caption + Environment.NewLine + Description;
 
-        public NewsModel(string caption, string description, string link)
+        public NewsModel(string caption, string description, string linkPath)
         {
+            Uri result;
+            if (!Uri.TryCreate(linkPath, UriKind.Absolute, out result))
+            {
+                result = new Uri("about:blank");
+            }
+            this.Link = result;
+
             this.Caption = caption;
             this.Description = description;
-            this.Link = link;
         }
     }
 }

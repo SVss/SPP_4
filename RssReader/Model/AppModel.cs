86717f3c71ad33281b936b15aba42a741ee9cd4d
SVss <savionak@gmail.com>
2016-12-06 21:30:29 +0300

    Add AppModel with list of users

---
+++ RssReader/Model/AppModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using RssReader.Utils;
+
+namespace RssReader.Model
+{
+    class AppModel
+    {
+        public List<UserModel> UsersList { get; private set; } = new List<UserModel>();
+
+        public UserModel AddUser(string name, int threadsCount)
+        {
+            var result = new UserModel(name, threadsCount);
+
+            UsersList.Add(result);
+
+            return result;
+        }
+
+        public void RemoveUser(UserModel user)
+        {
+            UsersList.Remove(user);
+        }
+
+        public void LoadUsersFromXml(XmlDocument document)
+        {
+            XmlElement root = document.FirstChild as XmlElement;
+            if ((root == null) || (root.Name != ConfigConsts.RootTag))
+            {
+                throw new BadXmlException();
+            }
+
+            foreach (XmlElement user in root.ChildNodes)
+            {
+                UsersList.Add(UserModel.FromXmlElement(user));
+            }
+        }
+
+        public XmlDocument SaveUsersToXml()
+        {
+            XmlDocument result = new XmlDocument();
+
+            XmlElement root = result.CreateElement(ConfigConsts.RootTag);
+            foreach (UserModel user in UsersList)
+            {
+                root.AppendChild(user.ToXmlElement(result));
+            }
+
+            result.AppendChild(root);
+
+            return result;
+        }
+    }
+}
